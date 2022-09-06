65d36ffdf3838d55b5fa6d7fb24dffe38f796802
Raashid Muhammed <raashidmuhammed@zilogic.com>
2022-09-06 13:44:46 +0530

    Demonstrate memory corruption.

--- fifo.c
+++ fifo.c
@@ -1,9 +1,20 @@
 #include <fifo.h>
 #include <stm.h>
 
+#include <string.h>
 #include <stdio.h>
 
 void fifo_test(fifo_page_t_v1 *fp)
 {
-	printf("Size @fifo(): %lu\n", sizeof(*fp));
+	int i;
+
+	printf("\nSize @fifo(): %lu\n", sizeof(*fp));
+
+	memset(fp, 0, sizeof(*fp));
+
+	for(i = 0; i < sizeof(*fp); i++) {
+		if ((i % 8) == 0)
+			printf("\n");
+		printf("%02x ", ((uint8_t *)fp)[i]);
+	}
 }
--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stm.h>
 #include <fifo.h>
 
+#include <string.h>
 #include <stdio.h>
 
 fifo_page_t_v1 fp;
+uint8_t arr[8];
 
 int main(void)
 {
+	int i;
+
 	printf("Size @main(): %lu\n", sizeof(fp));
 
+	memset(arr, 0xa5, sizeof(arr));
+	printf("\nArray before corruption:\n");
+	for (i = 0; i < 8; i++)
+		printf("Arr[%d]: %02X\n", i, arr[i]);
+
 	fifo_test(&fp);
+
+	printf("\n\nArray after corruption:\n");
+	for (i = 0; i < 8; i++)
+		printf("Arr[%d]: %02X\n", i, arr[i]);
 
 	return 0;
 }
